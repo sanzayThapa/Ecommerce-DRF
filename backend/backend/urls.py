76150708ae5518114018c7c84cc7b8241534f4a3
sanjay641 <sanjaythapa9808@gmail.com>
2024-05-31 18:19:02 +0545

    setup models

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('api/v1', include('djoser.urls.authtoken')),
 
 
-]
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
